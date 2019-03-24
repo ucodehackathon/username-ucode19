dbc0d5967d63de4e40e83bb219e62faf7b6e8919
Fran Menéndez Moya <fmenendezmoya@gmail.com>
2019-03-24 03:52:04 +0100

    UPDATE - changed hashcode to ucode :thinking_face:

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hashcode2019'`, () => {
+  it(`should have as title 'ucode2019'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('hashcode2019');
+    expect(app.title).toEqual('ucode2019');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to hashcode2019!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ucode2019!');
   });
 });
